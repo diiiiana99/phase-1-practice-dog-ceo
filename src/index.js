cd33151194fa20d0412fd6dd86ca1c8946d364a1
diiiiana99 <93669061+diiiiana99@users.noreply.github.com>
2022-01-19 12:55:07 -0500

    commit tree 7d033ff63b401b4af68f8c4c1a35c1bfbe69ae65

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 console.log('%c HI', 'color: firebrick')
+
+    
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(r => r.json())
+    .then(json => renderImages(json))
+    .catch(error => console.error(error))
+    
+
+    function renderImages (images) {
+
+        const imageContainer = document.getElementById('dog-image-container');
+        images.message.forEach( dog => {
+        const dogImg = document.createElement('img')
+        dogImg.src = dog;
+        imageContainer.appendChild(dogImg);
+    });
+    }
+
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(r => r.json())
+    .then(json => renderBreeds(json))
+    .catch(error => console.error(error))
+
+    function renderBreeds (breeds) {
+        const dogBreeds = document.getElementById('dog-breeds');
+        for( const dog in breeds.message ) {
+            const dogBreed = document.createElement('li')
+            dogBreed.textContent = dog;
+            dogBreeds.appendChild(dogBreed);
+        };
+    }
+  
+document.addEventListener ('click' , function (e) {
+    e.target.style.color = "pink"
+}
+) 
